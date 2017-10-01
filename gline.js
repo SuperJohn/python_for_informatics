e6401846868ebc4df5f2af74acf32aca8f9e42bd
John Houghton <johnkirkhoughton1@gmail.com>
2017-10-01 12:55:50 -0700

    adding files

---
+++ gline.js
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>300 Multiple Choices</title>
+</head><body>
+<h1>Multiple Choices</h1>
+The document name you requested (<code>/code/gmane/gline.js</code>) could not be found on this server.
+However, we found documents with names similar to the one you requested.<p>Available documents:
+<ul>
+<li><a href="/code/gmane/gline.htm">/code/gmane/gline.htm</a> (common basename)
+<li><a href="/code/gmane/gline.jsonp">/code/gmane/gline.jsonp</a> (common basename)
+<li><a href="/code/gmane/gline.py">/code/gmane/gline.py</a> (common basename)
+</ul>
+Please consider informing the owner of the <a href="http://www.py4inf.com/code/gmane/gline.htm">referring page</a> about the broken link.
+</body></html>
---
+++ gword.js
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>300 Multiple Choices</title>
+</head><body>
+<h1>Multiple Choices</h1>
+The document name you requested (<code>/code/gmane/gword.js</code>) could not be found on this server.
+However, we found documents with names similar to the one you requested.<p>Available documents:
+<ul>
+<li><a href="/code/gmane/gword.htm">/code/gmane/gword.htm</a> (common basename)
+<li><a href="/code/gmane/gword.jsonp">/code/gmane/gword.jsonp</a> (common basename)
+<li><a href="/code/gmane/gword.py">/code/gmane/gword.py</a> (common basename)
+</ul>
+Please consider informing the owner of the <a href="http://www.py4inf.com/code/gmane/gword.htm">referring page</a> about the broken link.
+</body></html>
