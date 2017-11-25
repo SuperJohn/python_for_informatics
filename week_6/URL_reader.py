9535256a500244987497893ad951abcc6114286b
John Houghton <johnkirkhoughton1@gmail.com>
2017-11-25 15:32:32 -0800

    completed week 6 assignment

--- week_6/URL_reader.py
+++ week_6/URL_reader.py
@@ -1,13 +1,23 @@
-import socket
+# author = John Houghton
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('www.py4inf.com', 80))
-mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+import urllib
 
-while True:
-    data = mysock.recv(512)
-    if ( len(data) < 1 ) :
-        break
-    print data;
+# url = 'http://www.py4inf.com/code/romeo.txt'
+url = raw_input('Enter URL read from > ')
 
-mysock.close()
+try:
+    fhand = urllib.urlopen(url)
+except:
+    print 'File cannot be opened: \"' + url + '\" Maybe try adding \"http://\" ?'
+    exit()
+
+print '\n'
+characters = 0    
+for line in fhand:
+    line = line.strip()
+    chars = len(line)
+    characters += chars
+    if characters < 3000:
+        print line
+
+print '\n<<< Exiting. The complete document had ' + str(characters) + ' characters, exceeding the 3k character print limit. >>>\n'
